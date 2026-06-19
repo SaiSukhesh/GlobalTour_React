0b328c8a347a78c34030fefec96c8a8788ef48ca
Sai Sukhesh <saisukheshreddy@gmail.com>
2026-06-19 10:46:13 +0530

    fix github pages path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Global_React/',
+  base: '/GlobalTour_React/',
   plugins: [react()],
 })
